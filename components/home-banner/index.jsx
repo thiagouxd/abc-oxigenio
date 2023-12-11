f80a04e1dd219959c5e0cc892241b51a1cba6460
thiago-menezes <thiago.menezes@gruposbf.com.br>
2023-12-11 11:44:16 -0300

    novo banner

--- components/home-banner/index.jsx
+++ components/home-banner/index.jsx
@@ -1,49 +1,59 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useCallback, useEffect, useState } from "react"
-import style from "./index.module.scss"
-import { ChevronsRight, ChevronsLeft, ArrowRight, ArrowLeft } from "react-feather"
+import React, { useCallback, useEffect, useState } from "react";
+import style from "./index.module.scss";
+import {
+  ChevronsRight,
+  ChevronsLeft,
+  ArrowRight,
+  ArrowLeft,
+} from "react-feather";
 
 const HomeBanner = ({ phone }) => {
-  const [carrossel, setCarrossel] = useState()
-  const [carrosselSize, setCarrosselSize] = useState("")
+  const [carrossel, setCarrossel] = useState();
+  const [carrosselSize, setCarrosselSize] = useState("");
   var interval;
 
   useEffect(() => {
     const element = document.getElementById("carrossel");
-    setCarrossel(element)
-    setCarrosselSize(element.offsetWidth)
-    interval = setInterval(() => { changeBannersAuto(element) }, 7000)
-  }, [])
-
-  const changeBannersAuto = (element) => {
-    if (element.scrollLeft === element.offsetWidth) {
-      scrollToLeftAuto(element)
-    } else {
-      scrollToRightAuto(element)
-    }
-  }
-
-  const scrollToRightAuto = (element) => {
-    element.scrollLeft = element.offsetWidth
-  }
-
-  const scrollToLeftAuto = (element) => {
-    element.scrollLeft = - element.offsetWidth
-  }
+    setCarrossel(element);
+    setCarrosselSize(element.offsetWidth);
+  }, []);
 
   const scrollToRight = () => {
+    carrossel.scrollLeft = carrossel.scrollLeft + carrosselSize;
     clearInterval(interval);
-    carrossel.scrollLeft = carrosselSize
-  }
+  };
 
   const scrollToLeft = () => {
+    carrossel.scrollLeft = carrossel.scrollLeft - carrosselSize;
     clearInterval(interval);
-    carrossel.scrollLeft = - carrosselSize
-  }
+  };
 
   return (
     <div className={style.containerCarrossel}>
       <div id="carrossel" className={style.carrossel}>
+        <div
+          style={{ cursor: "pointer" }}
+          className={style.banner}
+          onClick={() =>
+            window.open(
+              "https://docs.google.com/forms/d/e/1FAIpQLSdWgrxc8JGZp5GQwZCPjmb6_AXJlqk9jrrAUkv7XUBNaFCF-Q/viewform"
+            )
+          }
+        >
+          <h1 className={style.title}>
+            <span>
+              <strong>
+                Você está <br />
+                Satisfeito(a) <br />
+                com nosso <br />
+                atendimento?
+              </strong>
+            </span>
+          </h1>
+          <div className={style.containerText}></div>
+          <img className={style.image} src="banner4.png" />
+        </div>
         <div className={style.banner}>
           <h1 className={style.title}>
             <span>
@@ -53,24 +63,26 @@ const HomeBanner = ({ phone }) => {
           </h1>
           <div className={style.containerText}></div>
           <img className={style.image} src="banner1.png" />
-
-
         </div>
         <div className={style.banner}>
           <h1 className={`${style.title} ${style.titleSecondary}`}>
             <span>
-              <strong>Serviço de Home care
-                para concentradores de ar. </strong>
+              <strong>Serviço de Home care para concentradores de ar. </strong>
               Nós levamos até você.
             </span>
           </h1>
-          <div className={`${style.containerText} ${style.containerTextSecondary}`}></div>
+          <div
+            className={`${style.containerText} ${style.containerTextSecondary}`}
+          ></div>
           <img className={style.image} src="banner3.png" />
         </div>
       </div>
-      <a target="_blank" rel="noopener noreferrer"
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
         href={`https://api.whatsapp.com/send?phone=+55${phone}`}
-        className={style.button}>
+        className={style.button}
+      >
         <img src="logo-whats.svg" />
       </a>
 
@@ -83,7 +95,7 @@ const HomeBanner = ({ phone }) => {
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeBanner
+export default HomeBanner;
